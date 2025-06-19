1cfed7a879e6a5b74bdb01fb2cc3d3146b9b8e38
babacandiaye <ndiaybabacarnjaay@gmail.com>
2025-06-19 13:04:40 +0000

    Fix: Implemente the missing loggic is src/lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,13 @@
-// Implement extract_tx_version function below
 pub fn extract_tx_version(raw_tx_hex: &str) -> Result<u32, String> {
-    todo!()
+    if raw_tx_hex.len() < 8 {
+        return Err("Transaction data too short".to_string());
+    }
+
+    let version_bytes = match hex::decode(&raw_tx_hex[..8]) {
+        Ok(bytes) => bytes,
+        Err(_) => return Err("Hex decode error".to_string()),
+    };
+
+    let version = u32::from_le_bytes(version_bytes.try_into().unwrap());
+    Ok(version)
 }
